7d71a461526fb8599de7f33c9a5216e5e5934d67
poojapauskar <git.poojapauskar@gmail.com>
2017-07-17 11:07:52 +0530

    one api added

--- index.php
+++ index.php
@@ -7,6 +7,50 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
   </head>
   <body>
+
+
+<?php
+
+session_start();
+
+if(isset($_POST['login_btn'])){
+$url2 = 'https://reimburse.herokuapp.com/check_org_login/';
+$options2 = array(
+  'http' => array(
+    'header'  => array(
+                  'USERNAME: '.$_POST['username'],
+                  'PASSWORD: '.$_POST['password'],
+                ),
+    'method'  => 'GET',
+  ),
+);
+$context2 = stream_context_create($options2);
+$output2 = file_get_contents($url2, false,$context2);
+/*echo $output2;*/
+$arr2 = json_decode($output2,true);
+if($arr2['status']==200 && $arr2['message']=='Is Super Admin'){
+  $_SESSION['login_reimburse_app'] = 1;
+  echo "<script>location='super_admin.php'</script>";
+
+}elseif($arr2['status']==200 && $arr2['message']=='Is Admin'){
+  $_SESSION['login_reimburse_app'] = 1;
+  $_SESSION['is_admin'] = 1;
+  $_SESSION['admin_pk'] = $arr2['admin_pk'];
+  $_SESSION['account_token'] = $arr2['account_token'];
+  /*echo $_SESSION['account_token'];*/
+
+  echo "<script>location='home.php'</script>";
+
+}elseif($arr2['status']==401){
+  echo "<script type='text/javascript'>window.onload = function(){ alert('Password Invalid');}</script>";
+}elseif($arr2['status']==400){
+  echo "<script type='text/javascript'>window.onload = function(){ alert('Invalid Credentials');}
+</script>";
+}
+
+}
+?>
+
     <!-- Always shows a header, even in smaller screens. -->
   
     <div class="demo-layout-transparent mdl-layout mdl-js-layout">
@@ -46,7 +90,7 @@
           </p>
 
           <br>
-          <button name="submit" type="submit" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect">
+          <button name="login_btn" id="login_btn"  type="submit" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect">
            Log In
           </button>
 
