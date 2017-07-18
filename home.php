2df0a8cbd32a1af82ecc6b29b2d2836edc8ee0f0
Aiffin <aiffinsk@gmail.com>
2017-07-18 13:45:06 +0530

    landing page alignment working in firefix

--- home.php
+++ home.php
@@ -22,7 +22,7 @@ if($_SESSION['login_reimburse_app'] == 1){
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <!-- TABLE CSS AND JS -->
      <script src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-    <link rel="stylesheet" src="https://cdn.datatables.net/1.10.15/css/dataTables.material.min.css">
+   <!--  <link rel="stylesheet" src="https://cdn.datatables.net/1.10.15/css/dataTables.material.min.css"> -->
     <script src="//code.jquery.com/jquery-1.12.4.js"></script>
     <script src="https://cdn.datatables.net/1.10.15/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.10.15/js/dataTables.material.min.js"></script>
@@ -45,9 +45,9 @@ if($_SESSION['login_reimburse_app'] == 1){
     <html>
   <head>
     <!-- Material Design Lite -->
-    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <!-- <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
     <script src="https://cdn.datatables.net/1.10.7/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.0/js/materialize.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.0/js/materialize.min.js"></script> -->
     <!-- Material Design icon font -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <style type="text/css">
