8c5f7ec15aeae2604bd4ffbf7b097ce4f94e556e
Aiffin <aiffinsk@gmail.com>
2017-07-19 14:58:54 +0530

    fetched code1

--- home.php
+++ home.php
@@ -47,11 +47,7 @@ if($_SESSION['login_reimburse_app'] == 1){
     <html>
   <head>
     <!-- Material Design Lite -->
-
-
     <!-- <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
->>>>>>> landing page alignment working in firefix
->>>>>>> resolved conflict2
     <script src="https://cdn.datatables.net/1.10.7/js/jquery.dataTables.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.0/js/materialize.min.js"></script> -->
     <!-- Material Design icon font -->
