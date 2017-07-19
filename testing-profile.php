522b600649cc34c2d1d6c8d82ad25e3f28848220
mdshahrouq <mdshah1994@gmail.com>
2017-07-19 12:48:16 +0530

    Testing profile page demo

--- testing-profile.php
+++ testing-profile.php
@@ -162,16 +162,17 @@ $trip_id=$_GET['trip_id'];
 	function display_text(category,amount,img12){
       document.getElementById('category1').innerHTML=category;
       document.getElementById('amount1').innerHTML=amount;
+      document.getElementById('right_image').width="100%";
       document.getElementById('right_image').src=img12;
       /*alert(category);*/
 	}
 </script>
 			<?php for($t=0; $t< count($arr_get_a_user['categories']);$t++){?>
-		    <button style="width:100%;background:transparent;border:1px solid #D0D0D0 " onClick="display_text('<?php echo $arr_get_a_user['categories'][$t]['trip_details']['category'] ?>','<?php echo $arr_get_a_user['categories'][$t]['trip_details']['amount'] ?>','<?php echo $arr_get_a_user['categories'][$t]['image_id']; ?>')">
+		    <button onClick="display_text('<?php echo $arr_get_a_user['categories'][$t]['trip_details']['category'] ?>','<?php echo $arr_get_a_user['categories'][$t]['trip_details']['amount'] ?>','<?php echo $arr_get_a_user['categories'][$t]['image_id']; ?>')">
 				  <li class="mdl-list__item mdl-list__item--two-line ">
-				    <a style="text-align:left" class="mdl-list__item-primary-content " href="javascript:show_image();">
-				      <span style="text-align:left"><?php echo $arr_get_a_user['categories'][$t]['trip_details']['name']; ?></span>
-				      <span style="text-align:left" class="mdl-list__item-sub-title"><i><?php echo $arr_get_a_user['categories'][$t]['trip_details']['category']; ?></i></span>
+				    <a class="mdl-list__item-primary-content " href="javascript:show_image();">
+				      <span><?php echo $arr_get_a_user['categories'][$t]['trip_details']['name']; ?></span>
+				      <span class="mdl-list__item-sub-title"><i><?php echo $arr_get_a_user['categories'][$t]['trip_details']['category']; ?></i></span>
 				    </a>
 				    <span class="mdl-list__item-secondary-content">
 				      <span class="mdl-list__item-secondary-info">&#8377;<?php echo $arr_get_a_user['categories'][$t]['trip_details']['amount']; ?></span>
