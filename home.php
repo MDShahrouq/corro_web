ef78a4a587ca8b18d469fcef9966cfed49792d9a
Aiffin <aiffinsk@gmail.com>
2017-07-19 11:36:03 +0530

    resolved conflict in table of home page

--- home.php
+++ home.php
@@ -45,6 +45,7 @@ if($_SESSION['login_reimburse_app'] == 1){
     <html>
   <head>
     <!-- Material Design Lite -->
+
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
     <script src="https://cdn.datatables.net/1.10.7/js/jquery.dataTables.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.0/js/materialize.min.js"></script>
