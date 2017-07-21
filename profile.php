03906afeb457a843ef46fb62d772f19bef9c19a8
mdshahrouq <mdshah1994@gmail.com>
2017-07-21 15:07:22 +0530

    Slider working in firefox

--- profile.php
+++ profile.php
@@ -345,11 +345,7 @@ $trip_id=$_GET['trip_id'];
     </script>
 
 <script type="text/javascript">
-// function show_image(bill_image) {
-// 	var bill_img = document.createElement("bill_img");
-// 	bill_img.bill_image=bill_image;
-// document.getElementById('display_bill').innerHTML="<img src="+bill_img+" style="width:100%;" />";
-// }
+
 function goBack() 
 {
  window.history.back()
