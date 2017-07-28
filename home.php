6cb4af52bf266c1d94eb401198ea116e2f2fd9e1
Aiffin <aiffinsk@gmail.com>
2017-07-28 12:13:17 +0530

    Added the pie chart for approved and pending

--- home.php
+++ home.php
@@ -257,15 +257,19 @@ session_start();
 
 <!--===================================================================-->
     <div class="mdl-grid">
-    <div class="mdl-components mdl-js-components mdl-cell mdl-cell--4-col mdl-cell--4-col-tablet mdl-cell--4-col-desktop"></div>
-    <div class="mdl-components mdl-js-components mdl-cell mdl-cell--6-col mdl-cell--6-col-tablet mdl-cell--6-col-desktop">
+    <div class="mdl-components mdl-js-components mdl-cell mdl-cell--2-col mdl-cell--2-col-tablet mdl-cell--2-col-desktop"></div>
+    <div class="mdl-components mdl-js-components mdl-cell mdl-cell--4-col mdl-cell--4-col-tablet mdl-cell--4-col-desktop">
     <div id="piechart" style="width: 500px; height:500px;"></div>
     </div>
-     <div class="mdl-components mdl-js-components mdl-cell mdl-cell--4-col mdl-cell--4-col-tablet mdl-cell--4-col-desktop"></div>
+      <div class="mdl-components mdl-js-components mdl-cell mdl-cell--4-col mdl-cell--4-col-tablet mdl-cell--4-col-desktop">
+    <div id="piechart1" style="width: 500px; height:500px;"></div>
+    </div>
+
+     <div class="mdl-components mdl-js-components mdl-cell mdl-cell--2-col mdl-cell--2-col-tablet mdl-cell--2-col-desktop"></div>
      </div>
 
   <!-- ========================================================= -->
-  <div class="mdl-grid" style="margin-top: -276px;">
+  <div class="mdl-grid" style="margin-top: -276px;z-index:2;position:relative;">
           <!-- <div class="mdl-components mdl-js-components mdl-cell mdl-cell--2-col mdl-cell--2-col-tablet mdl-cell--2-col-desktop">
           <p>jsbksdjbvljdsbvljsd</p>
 
@@ -370,6 +374,36 @@ session_start();
         var chart = new google.visualization.PieChart(document.getElementById('piechart'));
 
         chart.draw(data, options);
+      }
+</script>
+<script type="text/javascript">
+        google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+
+        var data = google.visualization.arrayToDataTable([
+          ['Task', 'Hours per Day'],
+          ['Approved',  <?php echo $arr_get_a_org['approved_trips']; ?>],
+          ['Pending', <?php echo $arr_get_a_org['pending_trips']; ?>]
+         // ['Sleep',    <?php echo $arr_get_a_org['total_trips']; ?>]
+        ]);
+
+        var options = {
+          title: 'OVERVIEW',
+          backgroundColor:'#e2e1e0',
+          pieSliceText:'value',
+          sliceVisibilityThreshold :0,
+          legend:{position: 'center'},
+          chartArea:{top:0,width:"500px",height:"500px"},
+          tooltip:'value'
+
+
+        };
+
+        var chart1 = new google.visualization.PieChart(document.getElementById('piechart1'));
+
+        chart1.draw(data, options);
       }
 </script>
 <!-- 
